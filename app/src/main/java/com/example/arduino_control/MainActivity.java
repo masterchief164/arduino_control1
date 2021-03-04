7beaba9a44973fa6f54a33d146861497e6c273cf
masterchief164 <rishugupta2001.rg@gmail.com>
2021-03-05 00:01:24 +0530

    small progress

--- app/src/main/java/com/example/arduino_control/MainActivity.java
+++ app/src/main/java/com/example/arduino_control/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.arduino_control;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SeekBar seekBar_led = findViewById(R.id.seekBar_led);
+        SeekBar seekBar_fan = findViewById(R.id.seekBar_fan);
+        TextView textViewFan = findViewById(R.id.textView_fan);
+        TextView textViewLed = findViewById(R.id.textView_led);
+
+        seekBar_led.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float k = (float) (100.0 * progress / 256);
+                textViewLed.setText((getString(R.string.size_is) + k));
+                textViewLed.setTextSize(k);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
